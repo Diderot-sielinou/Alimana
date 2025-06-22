8535bde2c72356206adb4d9292cb16ab1d111413
diderot-sielinou <diderotsielinou@gmail.com>
2025-06-22 18:30:20 +0100

    add github action

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,25 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
   rules: {
-    'prettier/prettier': 'error',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
 };
