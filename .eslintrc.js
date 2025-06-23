15492b9f6f61f6557114c13788f7fe8a080d77c3
diderot-sielinou <diderotsielinou@gmail.com>
2025-06-23 14:08:47 +0100

    check eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,9 @@
 module.exports = {
-  extends: [
-    'next/core-web-vitals',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  plugins: ['@typescript-eslint'],
-  rules: {
-    'no-console': ['error', { allow: ['warn', 'error'] }],
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -17,9 +13,52 @@ module.exports = {
       jsx: true,
     },
   },
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
+  plugins: ['@typescript-eslint', 'import', 'unused-imports', 'jsx-a11y', 'prettier'],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // Console
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+
+    // Prettier
+    'prettier/prettier': 'warn',
+
+    // TypeScript
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { args: 'after-used', ignoreRestSiblings: true, argsIgnorePattern: '^_' },
+    ],
+
+    // Unused Imports
+    'unused-imports/no-unused-imports': 'warn',
+    'unused-imports/no-unused-vars': 'off',
+
+    // Import sorting
+    'import/order': [
+      'warn',
+      {
+        groups: ['type', 'builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        pathGroups: [
+          {
+            pattern: '~/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
   },
 };
