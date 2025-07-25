d9bd2f6f901d3319a2c406715a159d1efb69f018
Beleke Ian <ianseeksbeta@gmail.com>
2025-07-25 12:58:48 +0100

    Refactorisation of the sign in form to use formik and yup for form validation

--- src/schema/validation-schema.ts
+++ src/schema/validation-schema.ts
@@ -25,4 +25,5 @@ export const registerValidationSchema = Yup.object().shape({
 
 export const loginValidationSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email').required('Email is required'),
+  password: Yup.string().required('Password is required'),
 });
