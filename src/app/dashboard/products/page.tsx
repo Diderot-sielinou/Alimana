053d0f373860bfac767467d599b43a35d2c102ab
mombeh nadine <nadinemombeh@gmail.com>
2025-07-25 15:58:34 +0100

    commit changes made in my add product page

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -44,10 +44,8 @@ export default function ProductsPage() {
 
   return (
     <div className="flex min-h-screen relative">
-      {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} links={sidebarLinks} />
 
-      {/* Main Content */}
       <main className="flex-1 p-6 ml-0 md:ml-64">
         <div className="mb-4">
           <button
