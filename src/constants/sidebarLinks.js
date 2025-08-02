a167e7d7540e3092749970b38c0f8f86b42db40f
leila237 <mbiydzenyuyeileen@gmail.com>
2025-08-02 09:32:14 +0100

    made a fix on the sidebarLinks to seperate the roles from cash register object

--- src/constants/sidebarLinks.js
+++ src/constants/sidebarLinks.js
@@ -44,12 +44,15 @@ export const sidebarLinks = [
     requiredPermissions: [],
   },
   {
-    href: '/dashboard/manage-cash-register',
-    label: 'Cash Register',
-    icon: CreditCard,
     href: '/dashboard/roles',
     label: 'Roles',
     icon: Badge,
+    requiredPermissions: [],
+  },
+  {
+    href: '/dashboard/manage-cash-register',
+    label: 'Cash Register',
+    icon: CreditCard,
     requiredPermissions: [],
   },
   {
